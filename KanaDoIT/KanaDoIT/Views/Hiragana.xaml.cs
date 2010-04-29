5c64a49fe8ff0846313640cac4111bf7b585813c
Tim Callaghan <spoida@yahoo.com>
2010-04-29 10:55:14 +0000

    Initial checkin ok KanaDoIT

---
+++ KanaDoIT/KanaDoIT/Views/Hiragana.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+
+namespace KanaDoIT.Views
+{
+    public partial class Hiragana : Page
+    {
+        private Random random;
+        private DictionaryKanaInfo dictKana;
+        private KanaKey currentKanaKey;
+
+        public Hiragana()
+        {
+            InitializeComponent();
+            
+            random = new Random();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            dictKana = new DictionaryKanaInfo();
+
+            kanaPlaceholder.FontFamily = DictionaryKanaInfo.HiraganaFont;
+            kanaPlaceholder.FontSize = 200;
+
+            currentKanaKey = KanaKey.A;
+
+            kanaPlaceholder.Text = dictKana[currentKanaKey].FontCode;
+            romajiPlaceholder.Text = dictKana[currentKanaKey].Romaji;
+            SoundPlayer.Source = dictKana[currentKanaKey].SoundFilePath;
+        }
+
+        private void btnCycle_Click(object sender, RoutedEventArgs e)
+        {
+            currentKanaKey = (KanaKey)random.Next((int)KanaKey.N + 1);
+
+            kanaPlaceholder.Text = dictKana[currentKanaKey].FontCode;
+            romajiPlaceholder.Text = dictKana[currentKanaKey].Romaji;
+
+            SoundPlayer.Stop();
+            SoundPlayer.Source = dictKana[currentKanaKey].SoundFilePath;
+        }
+
+        private void btnReplay_Click(object sender, RoutedEventArgs e)
+        {
+            SoundPlayer.Stop();
+            SoundPlayer.Play();
+        }
+    }
+}
---
+++ KanaDoIT/KanaDoIT/Views/Nouns.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Windows.Navigation;
+using System.IO;
+using System.Windows.Markup;
+
+namespace KanaDoIT.Views
+{
+    public partial class Nouns : Page
+    {
+        private DictionaryNouns dictNouns;
+        private Random random;
+        private int nCurrentKey;
+
+        private List<NounInfo> listChoices;
+
+        public Nouns()
+        {
+            InitializeComponent();
+
+            listChoices = new List<NounInfo>();
+            random = new Random();
+        }
+
+        // Executes when the user navigates to this page.
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            dictNouns = new DictionaryNouns();
+
+            listChoices = dictNouns.GetRandonNounList();
+
+            nCurrentKey = random.Next(4);
+
+            var streamResourceInfo = Application.GetResourceStream
+                (
+                    listChoices[nCurrentKey].ImageFilePath
+                );
+
+            string xaml = null;
+
+            using (var resourceStream = streamResourceInfo.Stream)
+            {
+                using (var streamReader = new StreamReader(resourceStream))
+                {
+                    xaml = streamReader.ReadToEnd();
+                }
+            }
+
+            Canvas canvas = XamlReader.Load(xaml) as Canvas;
+
+            NounImage.Content = canvas;
+
+            btnChoiceA.Content = listChoices[0].GetNounKana();
+            btnChoiceB.Content = listChoices[1].GetNounKana();
+            btnChoiceC.Content = listChoices[2].GetNounKana();
+            btnChoiceD.Content = listChoices[3].GetNounKana();
+        }
+
+        private void btnChoice_Click(object sender, RoutedEventArgs e)
+        {
+            Button btnSender = sender as Button;
+
+            int nSelectedIndex = -1;
+            if (btnSender.Name == "btnChoiceA")
+            {
+                nSelectedIndex = 0;
+            }
+            else if (btnSender.Name == "btnChoiceB")
+            {
+                nSelectedIndex = 1;
+            }
+            else if (btnSender.Name == "btnChoiceC")
+            {
+                nSelectedIndex = 2;
+            }
+            else if (btnSender.Name == "btnChoiceD")
+            {
+                nSelectedIndex = 3;
+            }
+
+            if (nSelectedIndex != nCurrentKey)
+            {
+                Result.Text = "WRONG!";
+                btnSender.IsEnabled = false;
+            }
+            else
+            {
+                Result.Text = "CORRECT!";
+                btnChoiceA.IsEnabled = true;
+                btnChoiceB.IsEnabled = true;
+                btnChoiceC.IsEnabled = true;
+                btnChoiceD.IsEnabled = true;
+
+                listChoices = dictNouns.GetRandonNounList();
+
+                nCurrentKey = random.Next(4);
+
+                var streamResourceInfo = Application.GetResourceStream
+                    (
+                        listChoices[nCurrentKey].ImageFilePath
+                    );
+
+                string xaml = null;
+
+                using (var resourceStream = streamResourceInfo.Stream)
+                {
+                    using (var streamReader = new StreamReader(resourceStream))
+                    {
+                        xaml = streamReader.ReadToEnd();
+                    }
+                }
+
+                Canvas canvas = XamlReader.Load(xaml) as Canvas;
+
+                NounImage.Content = canvas;
+
+                btnChoiceA.Content = listChoices[0].GetNounKana();
+                btnChoiceB.Content = listChoices[1].GetNounKana();
+                btnChoiceC.Content = listChoices[2].GetNounKana();
+                btnChoiceD.Content = listChoices[3].GetNounKana();
+            }
+        }
+
+    }
+}
