0890e2ee9b38c37c4ba190dc2103e247dd4cebd7
Tim Callaghan <spoida@yahoo.com>
2010-07-03 14:23:10 +0000

    Removing auto generated host page and replacing with a new one that prevents browsers from caching the silverlight app and then not updating it when there are new versions

---
+++ KanaDoIT/KanaDoIT.Web/KanaDoIT.aspx.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Reflection;
+
+namespace KanaDoIT.Web
+{
+    public partial class KanaDoIT : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var versionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            SourceParam.Attributes["value"] += "?" + versionNumber;
+        }
+    }
+}
