57dbce8678b7a437801e704f5444bf137577b36b
Tim Callaghan <spoida@yahoo.com>
2010-07-04 12:54:18 +0000

    Adjusting project to not use elevated permissions so that auto update works (until I get some sort of signing sorted out)

--- KanaDoIT/KanaDoIT.Web/KanaDoIT.aspx.cs
+++ KanaDoIT/KanaDoIT.Web/KanaDoIT.aspx.cs
@@ -13,9 +13,11 @@ namespace KanaDoIT.Web
         protected void Page_Load(object sender, EventArgs e)
         {
             var versionNumber = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+            string strBasePath = Request.Url.AbsoluteUri.Replace("KanaDoIT.aspx", "");
             string strSilverlightSouceParam = String.Format
                 (
-                    "<param name=\"source\" value=\"ClientBin/KanaDoIT.xap?{0}\"/>",
+                    "<param name=\"source\" value=\"{0}ClientBin/KanaDoIT.xap?{1}\"/>",
+                    strBasePath,
                     versionNumber
                 );
             LiteralControl silverlightSouceParam = new LiteralControl(strSilverlightSouceParam);
